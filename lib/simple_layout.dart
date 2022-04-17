9418814f37be71b5a81b5830317e02789016878f
designus <info@designus.lt>
2022-04-17 19:59:31 +0200

    add text area and scroll view

--- lib/simple_layout.dart
+++ lib/simple_layout.dart
@@ -41,6 +41,36 @@ class SimpleLayout extends StatelessWidget {
     );
   }
 
+  Widget _buildText() {
+    return Container(
+      padding: const EdgeInsets.fromLTRB(16, 32, 16, 16),
+      child: const Text(
+          'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+          'Alps. Situated 1,578 meters above sea level, it is one of the '
+          'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+          'half-hour walk through pastures and pine forest, leads you to the '
+          'lake, which warms to 20 degrees Celsius in the summer. Activities '
+          'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+          'Alps. Situated 1,578 meters above sea level, it is one of the '
+          'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+          'half-hour walk through pastures and pine forest, leads you to the '
+          'lake, which warms to 20 degrees Celsius in the summer. Activities '
+          'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+          'Alps. Situated 1,578 meters above sea level, it is one of the '
+          'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+          'half-hour walk through pastures and pine forest, leads you to the '
+          'lake, which warms to 20 degrees Celsius in the summer. Activities '
+          'enjoyed here include rowing, and riding the summer toboggan run.'
+          'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+          'Alps. Situated 1,578 meters above sea level, it is one of the '
+          'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+          'half-hour walk through pastures and pine forest, leads you to the '
+          'lake, which warms to 20 degrees Celsius in the summer. Activities ',
+          softWrap: true,
+        ),
+    );
+  }
+
   Widget _buildActionButton(Color color, IconData icon, String label) {
     return Column(
       mainAxisSize: MainAxisSize.min,
@@ -82,12 +112,15 @@ class SimpleLayout extends StatelessWidget {
   Widget build(BuildContext context) {
     return Skeleton(
       title: 'Simple layout',
-      body: Column(
+      body: SingleChildScrollView(
+        child: Column(
         children: [
           _buildImage(),
           _buildTitleSection(context),
-          _buildActionButtons(context)
+          _buildActionButtons(context),
+          _buildText()
         ],
+      ),
       )
     );
   }
