688fc5c42d8dc74a312bf36b64939315c7015680
designus <info@designus.lt>
2022-04-18 16:54:13 +0200

    connect state to nav drawer

--- lib/nav_drawer.dart
+++ lib/nav_drawer.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:friendly_chat/models/chat_screen_model.dart';
 import 'package:friendly_chat/simple_layout.dart';
+import 'package:provider/provider.dart';
 import 'chat_screen.dart';
 import 'custom_layout.dart';
 
 class NavDrawer extends StatelessWidget {
+  const NavDrawer({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Drawer(
@@ -23,14 +27,24 @@ class NavDrawer extends StatelessWidget {
               // )
             ),
           ),
-          ListTile(
-            leading: const Icon(Icons.home),
-            title: const Text('Home page'),
-            onTap: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return const ChatScreen();
-              }));
-            },
+          Consumer<ChatScreenModel>(
+            builder: ((context, value, child) {
+              return ListTile(
+                leading: const Icon(Icons.chat),
+                title: Row(
+                  children: [
+                    const Text('Chat screen'),
+                    const SizedBox(width: 5),
+                    Text('(${value.messagesCount.toString()})')
+                  ],
+                ),
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return const ChatScreen();
+                  }));
+                },
+              );
+            }),
           ),
           ListTile(
             leading: const Icon(Icons.layers_outlined),
